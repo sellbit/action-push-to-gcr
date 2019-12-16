e2775b69897c63b3b31f9fa688009d40236b90a2
Jeff LaPorte <jeff.laporte@gmail.com>
2019-12-16 12:49:41 -0800

    Accept GCLOUD_SERVICE_KEY as base64

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
 
-echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
+echo $GCLOUD_SERVICE_KEY | base64 --decode | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
